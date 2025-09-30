120f9f688369e347eed5097c5ec1b22be961e4bf
514472 <514472@my.cwc.ac.uk>
2025-09-30 11:28:25 +0100

    spinbox

--- EXERCISE/Spinbox.py
+++ EXERCISE/Spinbox.py
@@ -1,18 +1,34 @@
 import tkinter as tk
 from tkinter import ttk
 
+def get_spinboxchange():
+  string_var.set( str(double_var.get()) )
+
 # This section creates the Tkinter Window and adds the required elements to it 
 window = tk.Tk()
 window. title('Tkinter Spinbox Widget')
-Window.geometry('400x400')
+window.geometry('400x400')
 
 # This is our Spinbox
-int_var = tk.IntVar()
-Number_Spinbox(window, from_=0, to=100, increment=.01, textvariable = int_var, font = 'Calibri 24 bold')
+double_var = tk.DoubleVar()
+number_Spinbox = ttk.Spinbox ( from_=0, to=100, increment=.01, textvariable = double_var, font = 'Calibri 24 bold', command= get_spinboxchange)
+
+# Create label
+string_var = tk.StringVar()
+output_label = ttk.Label(
+    window,
+    text='',
+    font=('Garamond', 20, 'bold'),
+    textvariable = string_var,
+    foreground='pink',
+    justify='center'
+)
 
 # Pack elements in frames ready to push onto form/window
-Number_spinbox.pack()
+number_Spinbox.pack()
+output_label.pack()
 
 # run the program to generate window with all placked elements for user interaction
 window.mainloop()
+
 
